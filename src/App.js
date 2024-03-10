191cb715ffab215a7da7dcf486f2b940ad847335
venkatpmv16 <mail2venkatpm@gmail.com>
2024-03-10 13:49:34 +0530

    adding styles to react elements

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ function App() {
     title: 'React Application',
     description:'This blog speaks all about creating react application from scrap'
   }
+  const styles = {
+    margin: '16px',
+    padding: '16px',
+    boxSizing: 'border-box',
+    borderRadius: '5px',
+    boxShadow: '0 2px 5px #ccc'
+  }
   return (
     <div className="App">
-      <div>
+      <div style={styles}>
       <h3>{blogObj.title}</h3>
       <p>{blogObj.description}</p>
       </div>  
-      <hr></hr>
-      <div>
+      <div style={styles}>
       <h3>{blogObj.title}</h3>
       <p>{blogObj.description}</p>
       </div>
       <hr></hr>
-      <div>
+      <div style={styles}>
       <h3>{blogObj.title}</h3>
       <p>{blogObj.description}</p>
       </div>
-      <hr></hr>
       
      
     </div>
