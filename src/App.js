fdd8e0fde4892425146f2a5fd852c9ff870bd881
venkatpmv16 <mail2venkatpm@gmail.com>
2024-03-10 14:23:34 +0530

    Creating Elements dynamically

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import './App.css';
 
 function App() {
-  const blogObj = {
-    title: 'React Application',
-    description:'This blog speaks all about creating react application from scrap'
-  }
+  const blogArr = [
+    {
+      id:1,
+      title: 'React Application',
+      description:'This blog speaks all about creating react application from scrap'
+    },
+    {
+      id:2,
+      title: 'React Application',
+      description:'This blog speaks all about creating react application from scrap'
+    },
+    { 
+      id:3,
+      title: 'React Application',
+      description:'This blog speaks all about creating react application from scrap'
+    },
+    {
+      id:4,
+      title: 'React Application',
+      description:'This blog speaks all about creating react application from scrap'
+    }
+  ]
+
+  const blogCards = blogArr.map((item,pos)=>{
+    console.log(item)
+    return (
+      <div className='BlogCard' key={item.id}>
+      <p>{item.id}</p>
+      <h3>{item.title}</h3>
+      <p>{item.description}</p>
+      </div>   
+    )
+  })
   return (
     <div className="App">
-      <div className='BlogCard'>
-      <h3>{blogObj.title}</h3>
-      <p>{blogObj.description}</p>
-      </div>  
-      <div className='BlogCard'>
-      <h3>{blogObj.title}</h3>
-      <p>{blogObj.description}</p>
-      </div>
-      <hr></hr>
-      <div className='BlogCard'>
-      <h3>{blogObj.title}</h3>
-      <p>{blogObj.description}</p>
-      </div>
-      
-     
+      {blogCards}
     </div>
   );
 }
