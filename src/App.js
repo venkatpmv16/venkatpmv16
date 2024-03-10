21d4817dfefe17e4f4fab834ff45f85357ad4166
venkatpmv16 <mail2venkatpm@gmail.com>
2024-03-10 13:55:29 +0530

    Styling using CSS file

--- src/App.js
+++ src/App.js
@@ -5,25 +5,18 @@ function App() {
     title: 'React Application',
     description:'This blog speaks all about creating react application from scrap'
   }
-  const styles = {
-    margin: '16px',
-    padding: '16px',
-    boxSizing: 'border-box',
-    borderRadius: '5px',
-    boxShadow: '0 2px 5px #ccc'
-  }
   return (
     <div className="App">
-      <div style={styles}>
+      <div className='BlogCard'>
       <h3>{blogObj.title}</h3>
       <p>{blogObj.description}</p>
       </div>  
-      <div style={styles}>
+      <div className='BlogCard'>
       <h3>{blogObj.title}</h3>
       <p>{blogObj.description}</p>
       </div>
       <hr></hr>
-      <div style={styles}>
+      <div className='BlogCard'>
       <h3>{blogObj.title}</h3>
       <p>{blogObj.description}</p>
       </div>
